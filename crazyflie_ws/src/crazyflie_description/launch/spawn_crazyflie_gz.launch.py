ed0c764f92e10c1144aaa2f5b58bbcde81df1286
uzmah04 <zu.goomany@gmail.com>
2026-05-10 20:51:56 +0400

    Updated Launch file and ROS bridge to obtain pose and odometry values of the crazyflie

--- crazyflie_ws/src/crazyflie_description/launch/spawn_crazyflie_gz.launch.py
+++ crazyflie_ws/src/crazyflie_description/launch/spawn_crazyflie_gz.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
@@ -13,8 +13,23 @@ def generate_launch_description():
     pkg_share = get_package_share_directory("crazyflie_description")
     xacro_file = os.path.join(pkg_share, "urdf", "crazyflie_body.xacro")
 
+
+    # Paths to access existing crazyflie_world.sdf instead of empty.sdf
+    simulation_root = os.path.abspath(os.path.join(pkg_share, '../../../../../'))
+    gz_resource_path = os.path.join(simulation_root, 'simulator_files/gazebo')
+    world_file = os.path.join(gz_resource_path, 'worlds/crazyflie_world.sdf')
+
+
     # Process xacro → urdf
     robot_description_config = xacro.process_file(xacro_file).toxml()
+
+
+    # Environment variable for Gazebo
+    set_gz_path = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=gz_resource_path
+    )
+
 
     # Launch argument for robot name
     robot_name_arg = DeclareLaunchArgument(
@@ -22,6 +37,21 @@ def generate_launch_description():
         default_value="crazyflie",
         description="Name of the robot"
     )
+
+    # For Rviz
+    use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation time'
+    )
+    
+    namespace = LaunchConfiguration("robot_name")
+
+
+    """
+    Commented below code:
+        - will start existing crazyflie_world.sdf instead of an empty one
+        - since the world file already contains a crazyflie model, will not need to spawn the drone
 
     # Start Gazebo Harmonic (empty world)
     gazebo_launch = IncludeLaunchDescription(
@@ -46,7 +76,31 @@ def generate_launch_description():
         ],
         output="screen",
     )
+    """
+    # Gazebo with custom world
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py"
+            )
+        ),
+        launch_arguments={"gz_args": f"-r {world_file}"}.items(),
+    )
 
+
+    # Robot State Publisher (for RViz)
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'robot_description': robot_description_config
+        }]
+    )
+
+    
     # ROS-Gazebo bridge
     bridge = Node(
         package="ros_gz_bridge",
@@ -57,9 +111,12 @@ def generate_launch_description():
         output="screen",
     )
 
+
     return LaunchDescription([
+        set_gz_path,
         robot_name_arg,
+        use_sim_time,
         gazebo_launch,
-        spawn_robot,
+        robot_state_publisher,
         bridge,
     ])
